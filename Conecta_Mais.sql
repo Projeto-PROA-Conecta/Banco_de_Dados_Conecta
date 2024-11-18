815106cd2227dbad090e21651bcd204638bb1b0e
Davi_dev1 <davigancalveesdasilva@gmail.com>
2024-11-18 18:37:11 -0300

    Add files via upload

---
+++ Conecta_Mais.sql
@@ -0,0 +1,35 @@
+use conecta;
+CREATE DATABASE Conecta;
+
+
+CREATE TABLE Usuarios(
+usuario_id int auto_increment PRIMARY KEY,
+nome varchar(50) not null,
+email varchar(320) not null UNIQUE,
+telefone varchar(15) not null,
+senha varchar (255) not null,
+data_de_nascimento DATE not null,
+cpf varchar (14) not null UNIQUE,
+rg varchar (20) not null UNIQUE ,
+cep varchar (9) not null UNIQUE
+);
+
+
+Create TABLE Curriculo(
+curriculo_id int auto_increment primary Key,
+formacao_academica 	varchar(50),
+experiencia_profissional varchar(200),
+certificacoes varchar(150),
+habilidades varchar(200),
+usuario_id int,
+foreign key(usuario_id) references Usuarios(usuario_id)
+);
+
+Create TABLE condicao(
+condicao_id int auto_increment primary key,
+tipo_de_condicao varchar(50),
+nivel_da_condicao varchar(20),
+atestado_medico varchar(100),
+usuario_id int,
+foreign key (usuario_id) references Usuarios(usuario_id)
+);
