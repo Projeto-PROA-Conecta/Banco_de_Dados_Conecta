a0f6eefc6d8dac8d7a897e9e784445958366c8cb
Davi_dev1 <davigancalveesdasilva@gmail.com>
2024-12-09 00:06:38 -0300

    Delete conectamais/conectamais directory

--- conectamais/conectamais/src/main/java/br/com/conectamais/conectamais/repository/CurriculoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.conectamais.conectamais.repository;
-
-import br.com.conectamais.conectamais.models.Curriculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CurriculoRepository extends JpaRepository <Curriculo,UUID> {
-}
